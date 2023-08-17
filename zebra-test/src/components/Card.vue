d3f07168d84d5743b9d494e101fcf8e46eb80b6b
Vitaly Savinov <vitalanother@gmail.com>
2023-08-17 22:07:12 +0300

    improved layout, fixed fetch

--- zebra-test/src/components/Card.vue
+++ zebra-test/src/components/Card.vue
@@ -1,112 +1,54 @@
 <script setup>
-  import { ref } from "vue";
-  const image = ref(true)
+  import moment from 'moment';
+  const props = defineProps({
+    newsItem: Object
+  });
+
+  const humanDate =  new Date(props.newsItem.date * 1000);
+  const dateDay = moment(humanDate).format('DD');
+  const dateMonth = moment(humanDate).format('MMMM');
+  const dateYear = moment(humanDate).format('YYYY');
 </script>
 
 
 <template>
   <section class="card">
-    <div v-show="image" class="card__img-container">
-      <img src="/public/img/news-pic.jpg" alt="">
+    <div v-show="newsItem.image" class="card__img-container">
+        <img class="card__img" :src="newsItem.image" alt="News image">
     </div>
-    <div v-show="!image" style="height: 16px;"></div>
     <div class="card__container">
-      <div class="card__text">
-        <div class="card__date">
-          <div>
-            <span class="card__date-day" >28</span>
-          </div>
-          <div>
-            <span class="card__date-rest">February</span>
-            <span class="card__date-rest">2022</span>
-          </div>
+      <div v-show="newsItem.image" style="height:250px;flex-shrink:0"></div>
+      <div v-show="!newsItem.image" style="height: 16px;"></div>
+      <div class="card__date">
+        <div>
+          <span class="card__date-day" >{{ dateDay }}</span>
         </div>
-        <h4>Alpha сообщает о новых высокопроизводительных оборудованиях для сельского хозяйства</h4>
-        <p>Alpha объявляет о партнёрстве с Миром, ведущим мировым поставщиком экологических услуг, чтобы помочь клиентам строить светлое будущее.</p>
+        <div>
+          <span class="card__date-rest">{{ dateMonth }}</span>
+          <span class="card__date-rest">{{ dateYear }}</span>
+        </div>
       </div>
-      <div class="card__badge">Новости</div>
+      <h4>{{ newsItem.name }}</h4>
+      <p>{{ newsItem.previewText }}</p>
+    </div>
+    <div class="card__badge-container">
+      <div class="card__badge">{{ newsItem.type.value}}</div>
     </div>
   </section>
 </template>
 
 <style scoped>
-  .card {
-    position: relative;
-    width: 536px;
-    height: 577px;
-    border: 1px solid #0F62FE;
-    border-radius: 16px;
-    display: flex;
-    flex-direction: column;
-  }
-  .card__img-container {
-    margin-top: -1px;
-    margin-left: -1px;
-    width: 536px;
-    height: 250px;
-    border-radius: 16px 16px 0px 0px;
-  }
-  .card__img-container img{
-    width: 100%;
-    height: auto;
-  }
-  .card__container {
-    flex-grow: 1;
-    padding: 32px;
-    display: flex;
-    flex-direction: column;
-    align-items: baseline;
-  }
-  .card__text {
-    flex-grow: 1;
-  }
-  .card__date {
-    margin-bottom: 16px;
-    display: flex;
-    color: #A1A7B5;
-  }
-  .card__date-day {
-    font-size: 36px;
-    font-weight: 400;
-    line-height: 36px;
-    letter-spacing: 0em;
-    text-align: left;
-  }
-  .card__date-rest {
-    margin-left: 4px;
-    display: block;
-    font-size: 15px;
-    font-weight: 700;
-    line-height: 17px;
-    letter-spacing: -0.01em;
-    text-align: left;
-  }
-  .card h4 {
-    display: inline-block;
-    font-size: 22px;
-    font-weight: 400;
-    line-height: 26px;
-    letter-spacing: 0em;
-    text-align: left;
-    color: #0C5BEF;
-    margin-bottom: 16px;
-  }
-
-  .card h4:hover {
-    cursor: pointer;
-    color:#0029A9;
-  }
-  .card p {
-    display: inline-block;
-    font-size: 20px;
-    font-weight: 400;
-    line-height: 26px;
-    letter-spacing: -0.01em;
-    text-align: left;
-  }
-  .card__badge {
-    padding: 4px 16px 4px 16px;
-    border-radius: 360px;
-    background-color:#F0F6FE;
-  }
+.card {position: relative;cursor: pointer;}
+.card:hover h4 {color:#0029A9;}
+.card__container {width: 536px;height: 577px;border: 1px solid #0F62FE;border-radius: 16px;display: flex;flex-direction: column;padding: 32px;overflow: hidden;}
+.card__img-container {width: 536px;height: 250px;border-radius: 16px 16px 0px 0px; position: absolute;top: -1px;}
+.card__img {width: 100%;height: auto;border-radius: 16px 16px 0px 0px;}
+.card__date {margin-bottom: 16px;display: flex;color: #A1A7B5;}
+.card__date-day {font-size: 36px;font-weight: 400;line-height: 36px;letter-spacing: 0em;text-align: left;}
+.card__date-rest {margin-left: 4px;display: block;font-size: 15px;font-weight: 700;line-height: 17px;letter-spacing: -0.01em;text-align: left;}
+.card__badge-container {position: absolute;bottom: 1px;left:2px; padding: 0 32px; height: 92px; width: 99%; background-color: white;
+display: flex;align-items: center;border-radius: 16px; border: 2px solid transparent;}
+.card__badge {padding: 4px 16px 4px 16px;border-radius: 360px;background-color:#F0F6FE;}
+.card h4 {display: block;font-size: 22px;font-weight: 400;line-height: 26px;letter-spacing: 0em;text-align: left;color: #0C5BEF;margin-bottom: 16px;}
+.card p {display: block;font-size: 20px;font-weight: 400;line-height: 26px;letter-spacing: -0.01em;text-align: left;flex-shrink: 1;}
 </style>
--- zebra-test/src/components/NewsCatalog.vue
+++ zebra-test/src/components/NewsCatalog.vue
@@ -1,52 +1,35 @@
 <script setup>
 import Card from './card.vue';
+import {reactive, ref} from 'vue'
+
+const data = ref([])
+const error = ref([])
+
+let api = 'http://flems.github.io/test/api/news/';
+
+
+fetch(api)
+  .then((res) => res.json())
+  .then((json) => (data.value = json))
+  .catch((err) => (error.value = err))
 </script>
 
 <template>
   <section class="news">
     <div class="container">
       <div class="news__flex">
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+        <Card v-for="newsItem in data.items" :newsItem="newsItem"/>
       </div>
       <div class="news__button-container">
-        <button class="news__button">загрузить ещё</button>
+        <button v-show="(data.nav.current!==data.nav.total)"  class="news__button">загрузить ещё</button>
       </div>
     </div>
   </section>
 </template>
 
 <style scoped>
-  .news {
-    padding: 64px 0 72px 0;
-  }
-  .news__flex {
-    /* display: grid;
-    grid-template-columns: 536px 536px 536px; */
-    display: flex;
-    flex-wrap: wrap;
-    gap:64px 46px;
-    margin-bottom: 72px;
-  }
-  .news__button-container {
-    display: flex;
-    justify-content: center;
-  }
-  .news__button {
-    cursor: pointer;
-    display: inline-block;
-    font-size: 20px;
-    font-weight: 600;
-    line-height: 24px;
-    letter-spacing: -0.01em;
-    text-align: center;
-    padding: 16px 32px;
-    border: 1px solid #002DBF;
-    border-radius: 8px;
-    background-color: white;
-    color: #002DBF;
-  }
+  .news {padding: 64px 0 72px 0;}
+  .news__flex {display: flex;flex-wrap: wrap;gap:64px 46px;margin-bottom: 72px;}
+  .news__button-container {display: flex;justify-content: center;}
+  .news__button {cursor: pointer;display: inline-block;font-size: 20px;font-weight: 600;line-height: 24px;letter-spacing: -0.01em;text-align: center;padding: 16px 32px;border: 1px solid #002DBF;border-radius: 8px;background-color: white;color: #002DBF;}
 </style>
